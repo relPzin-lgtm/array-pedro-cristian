21a6b68b84668fe0565b95467cd1b55d68bc4ecf
relPzin-lgtm <agenilsonsales392019@gmail.com>
2025-09-01 20:04:24 -0300

    Codigo e Explicação Atividade de Estrutura de dados

---
+++ array.py
@@ -0,0 +1,8 @@
+#LISTA DE 5 ALUNOS
+amigos_de_sala = ["Ariel", "Itamar", "Mauricio", "Kaiky", "Roni"]
+# Acessando o primeiro nome
+print('primeiro nome: ', amigos_de_sala[0])
+# Acessando o terceiro nome
+print('terceiro nome: ', amigos_de_sala[2])
+#Acessando o Ultimo nome
+print('ultimo nome: ', amigos_de_sala[-1])
